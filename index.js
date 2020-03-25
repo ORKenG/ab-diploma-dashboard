ee9a7fcd1486ce9ffe2d1b45efb63486db447ab2
oburia4 <oleh.buria@capgemini.com>
2020-03-25 13:07:39 +0200

    initialized auth logic

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-
+require('rootpath')();
 const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('_helpers/jwt');
+const errorHandler = require('_helpers/error-handler');
 const path = require('path');
-const PORT = process.env.PORT || 5000;
-
-let app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs');
 
 
-  app.get('/', (req, res) => res.render('pages/index'));
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// use JWT auth to secure the api
+app.use(jwt());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+app.get('/', (req, res) => res.render('pages/index'));
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const server = app.listen(port, function () {
+    console.log('Server listening on port ' + port);
+});
