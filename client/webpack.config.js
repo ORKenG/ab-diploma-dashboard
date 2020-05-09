b86748327758082dc3d7db8e3975264d94899399
Yurii Karmazin <karmazin.yura@gmail.com>
2020-05-09 07:52:15 +0300

    - Added scss/css support ( extedned webpack config )

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,22 +1,37 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path');
+const isDevelopment = process.env.NODE_ENV === 'development'
 
 module.exports = {
     mode: 'development',
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: ['.js', '.jsx', '.scss']
     },
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
                 loader: 'babel-loader'
+            },
+            {
+                test: /\.module\.s(a|c)ss$/,
+                loader: [ isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        {loader: 'css-loader', options: { modules: true,sourceMap: isDevelopment }},
+                        { loader: 'sass-loader',options: {sourceMap: isDevelopment }} ]
+            },
+            {
+                test: /\.s(a|c)ss$/, exclude: /\.module.(s(a|c)ss)$/,
+                loader: [ isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    { loader: 'sass-loader', options: { sourceMap: isDevelopment }}  ]
             }
         ]
     },
-    plugins: [new HtmlWebpackPlugin({
-        template: './src/index.html'
-    })],
+    plugins: [
+        new HtmlWebpackPlugin({ template: './src/index.html' }),
+        new MiniCssExtractPlugin({filename: isDevelopment ? '[name].css' : '[name].[hash].css', chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'})
+    ],
     devServer: {
         historyApiFallback: true
     },
